4ddb8d3f632de0f3d357139c58610e0b4c6474a0
queshawn.hinton <queshawn.hinton@gmail.com>
2014-07-12 21:47:29 +0000

    comments can be submitted via ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,16 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create!(comment_params)
-    redirect_to @post
-  end
-  
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.js
+    end
+    
   private
   def comment_params
     params.require(:comment).permit(:body)
 end
 end
+
+
+  end
